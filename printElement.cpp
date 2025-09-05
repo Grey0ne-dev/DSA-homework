5b810f85fb17835babd42f11767fd6be77c91ad4
Grey0ne-dev <sergey2002gurgen@gmail.com>
2025-09-05 15:07:18 +0400

    Add files via upload

---
+++ printElement.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+template<typename T>
+
+
+void print(const T & obj) {
+    std::cout<<obj<<std::endl;
+}
+
+
+int main() {
+	
+	std::string str = "nigga";
+	int x = 69;
+	double y = 1.0978;
+	print(str);
+	print(x);
+	print(y);
+
+}
