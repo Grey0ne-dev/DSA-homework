4ec1c19db060cd6cb6b99b5b8456b26ac4d67ebd
Grey0ne-dev <sergey2002gurgen@gmail.com>
2025-09-07 15:45:23 +0400

    Add files via upload

---
+++ pair++.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T1, typename T2>
+
+class Pair {
+    T1 first;
+    T2 second;
+    public:
+    Pair(T1 first, T2 second) : first(first), second(second) {}
+    const T1 getFirst() {return first;}
+    const T2 getSecond() {return second;}
+    void setFirst(const T1 & arg) {first = arg;}
+    void setSecond(const T2 & arg) {second = arg;}
+    const void print() {cout << first << ':' << second << endl;}
+};
+
+template <typename T>
+class Pair <T,T>{
+    T first;
+    T second;
+    public:
+    Pair(T first, T second) : first(first), second(second) {}
+    const T getFirst() {return first;}
+    const T getSecond() {return second;}
+    void setFirst(const T & arg) {first = arg;}
+    void setSecond(const T & arg) {second = arg;}
+    const void print() { 
+        cout << "Pair of identical types: ...";
+        cout << first << ':' << second << endl;}
+    
+};
+
+
+
+
+
+int main() {
+	
+    Pair <double, double> coords (2, 2.87);
+    Pair  <string, short> codename ("flowers", 21);
+    Pair <char, int> model ('e', 320);
+    coords.print();
+    codename.print();
+    model.print();
+    return 0;
+}
